d5b26606657473dc3d49d97445957fa0ae1ad2e7
javim7 <61723252+javim7@users.noreply.github.com>
2021-03-23 15:19:02 -0600

    agregando metodos de setq

--- SetQueue.java
+++ SetQueue.java
@@ -15,11 +15,37 @@ public class SetQueue {
     //creando el hashmap tipo String, String
     HashMap<String, String> mapaLisp= new HashMap<String, String>();;
 
-    public void agregarValor(String llave, String valor) {
 
+    /** 
+     * Pre: Se ingresa el dato
+     * @param item se ingresa un dato para agregar al Vector
+     * Post: Se guarda el dato en Stack
+     */
+    public void agregarValor(String llave, String valor) {
 
         //agregando el valor y su llave al mapa
         mapaLisp.put(llave, valor);
+
+    }
+
+
+    /** 
+     * Pre: Se ingresa el dato
+     * @param item se ingresa un dato para agregar al Vector
+     * Post: Se guarda el dato en Stack
+     */
+    public void buscarValor(String valor) {
+
+    }
+
+
+
+    /** 
+     * Pre: Se ingresa el dato
+     * @param item se ingresa un dato para agregar al Vector
+     * Post: Se guarda el dato en Stack
+     */
+    public void cambiarValor(String valor) {
 
     }
     
