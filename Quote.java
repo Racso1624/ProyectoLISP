81c5ce7d15b237076206f8e8ba2b44dc666f31d5
bar20807 <70623939+bar20807@users.noreply.github.com>
2021-03-23 21:58:11 -0600

    Add files via upload

---
+++ Quote.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author José Rodrigo Barrera García 
+ * Carnet: 20807
+ * Universidad del Valle de Guatemala
+ *
+ */
+class Quote{
+
+	/**
+	 *
+	 * @param Instruccion que no se desea evaluar 
+	 * @return La misma instruccion a utilizar en el interprete
+	 */
+	public String DevolverQuote(String Instruccion){
+		System.out.println(Instruccion);
+		return Instruccion;
+	}
+}
